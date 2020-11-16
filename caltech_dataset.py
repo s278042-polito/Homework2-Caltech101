b8cbb7f24fbd6356cffce1d8ddc937ab41e05db0
s278042-polito <74179701+s278042-polito@users.noreply.github.com>
2020-11-16 14:58:20 +0100

    debug 1

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -30,7 +30,9 @@ def make_dataset(directory, split, class_to_idx):
         for root, _, filenames in sorted(os.walk(target_dir, followlinks=True)):
             for filename in sorted(filenames):
                 path = os.path.join(root, filename)
-                instance = '/'.join([root.split('\\')[1], filename])
+                print('root', root)
+                print('path', path)
+                # instance = '/'.join([root.split('\\')[1], filename])
                 if instance in split_instances:
                     item = path, class_index
                     instances.append(item)
