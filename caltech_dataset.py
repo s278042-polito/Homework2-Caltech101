cb7ed33419af78a23a3b4c0e3c82ed139ec3e954
Andrea Zappavigna <s278042@studenti.polito.it>
2020-11-16 12:38:05 +0100

    fix 2

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -17,7 +17,7 @@ def pil_loader(path):
 def make_dataset(directory, split, class_to_idx):
     instances = []
     directory = os.path.expanduser(directory)
-    split_filepath = os.path.join('Caltech101', split + '.txt')
+    split_filepath = os.path.join(split + '.txt')
     split_instances = open(split_filepath).readlines()
 
     for target_class in sorted(class_to_idx.keys()):
@@ -30,7 +30,8 @@ def make_dataset(directory, split, class_to_idx):
         for root, _, filenames in sorted(os.walk(target_dir, followlinks=True)):
             for filename in sorted(filenames):
                 path = os.path.join(root, filename)
-                if path in split_instances:
+                instance = '/'.join([root.split('\\')[1], filename])
+                if instance in split_instances:
                     item = path, class_index
                     instances.append(item)
     return instances
